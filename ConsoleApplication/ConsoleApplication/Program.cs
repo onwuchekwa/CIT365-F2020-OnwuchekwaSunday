0fe739de5c83878b857aa7eb3d70a995be2375cf
onwuchekwa <donsonde@gmail.com>
2020-09-17 01:01:39 +0000

    1. name and location variables update to accept input from user 2. ffunctionality to wait for user's response added

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -18,23 +18,53 @@ namespace ConsoleApplication
 
 
             // Store two variables: Your name and Your location(state or country)
-            String name = "Sunday Ogbonnaya Onwuchekwa", location = "Abia State, Nigeria";
+            String name, location;
 
             //Store Christmas date
-            DateTime christmasDate = new DateTime(2020, 12, 25);            
+            DateTime christmasDate = new DateTime(2020, 12, 25);
+
+            //Get name from user
+            Console.Write("\nWhat is your name? ");
+            name = Console.ReadLine();
+
+            //Get location from user
+            Console.Write($"\nHi {name}! Where are you from? ");
+            location = Console.ReadLine();
+
+            // Output a friendly message
+            Console.WriteLine($"I have never been to {location}. I bet it is a wonderful place to live. Press any key to continue...");
+
+            Console.ReadKey();
 
             // Output name and location
-            Console.WriteLine($"\nMy name is {name}.");
-            Console.WriteLine("I am from {0}.", location);
+            Console.WriteLine("\nThe line below is coded using String Interpolation:");
+
+            Console.WriteLine($"My name is {name}. I am from {location}.");
+
+            Console.WriteLine("Press any key to continue...");
+
+            Console.ReadKey();
+
+            Console.WriteLine("\n\n\nPress any key to continue...");
+
+            Console.ReadKey();
 
             // Output today's date
-            Console.WriteLine("\nToday's date is {0}.", DateTime.Now.ToString("MMMM dd, yyyy"));
+            Console.WriteLine($"\nToday's date is {DateTime.Now.ToString("MMMM dd, yyyy")}.");
+
+            Console.WriteLine("Press any key to continue...");
+
+            Console.ReadKey();
 
             // Calculate remaining days until christmas
             double daysToChristmas = christmasDate.Subtract(DateTime.Today).TotalDays;
 
             // Output number of days until christmas
             Console.WriteLine($"\nHooray! We have {daysToChristmas} days until christmas.");
+
+            Console.WriteLine("\nPress any key to continue...");
+
+            Console.ReadKey();
 
             // Add the program example from section 2.1 in the C# Programming Yellow Book by Rob Miles.
             Console.WriteLine("\n\n******************************************************************************************\n" +
@@ -48,12 +78,12 @@ namespace ConsoleApplication
             string widthString, heightString;
 
             // The program accepts the width of the window
-            Console.WriteLine("\nEnter the width of the window");
+            Console.Write("\nEnter the width of the window: ");
             widthString = Console.ReadLine();
             width = double.Parse(widthString);
 
             // The program accepts the height of the window
-            Console.WriteLine("\nEnter the height of the window");
+            Console.Write("\nEnter the height of the window: ");
             heightString = Console.ReadLine();
             height = double.Parse(heightString);
 
@@ -62,8 +92,8 @@ namespace ConsoleApplication
             glassArea = 2 * (width * height);
 
             // Output the length pf wood and area of glass required
-            Console.WriteLine("\nThe length of the wood is " + woodLength + " feet");
-            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+            Console.WriteLine($"\nThe length of the wood is  {woodLength} feet");
+            Console.WriteLine($"The area of the glass is  {glassArea} square metres");
 
             // Do not automatically terminate the console
             Console.ReadKey();
