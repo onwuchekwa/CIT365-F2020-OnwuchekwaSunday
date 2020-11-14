9b75df514b82aeba7eda65f794f0fbb07882f3f8
Sunday Onwuchekwa <donsonde@gmail.com>
2020-11-14 21:06:58 +0000

    First commit

---
+++ MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MvcMovieContext _context;
+        private readonly IWebHostEnvironment _environment;
+
+        public MoviesController(MvcMovieContext context, IWebHostEnvironment environment)
+        {
+            _context = context;
+            _environment = environment;
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index(string movieGenre, string searchString, string sortByDate)
+        {
+            ViewData["DateSort"] = sortByDate == "ReleasedDate" ? "SortDateDesc" : "ReleasedDate";
+
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.TItle.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            switch(sortByDate)
+            {
+                case "SortDateDesc":
+                    movies = movies.OrderByDescending(s => s.ReleaseDate);
+                    break;
+
+                default:
+                    movies = movies.OrderBy(s => s.ReleaseDate);
+                    break;
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
+        }
+
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TItle,ReleaseDate,Genre,Price,Rating,imagePath")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                var imageFiles = HttpContext.Request.Form.Files;
+
+                foreach(var imageFile in imageFiles)
+                {
+                    if (imageFile != null && imageFile.Length > 0)
+                    {
+                        var file = imageFile;
+                        var uploadImage = Path.Combine(_environment.WebRootPath, "images");
+
+                        if(file.Length > 0)
+                        {
+                            var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                            System.Console.WriteLine(fileName);
+                            using (var fileStream = new FileStream(Path.Combine(uploadImage, file.FileName), FileMode.Create))
+                            {
+                                await file.CopyToAsync(fileStream);
+                                movie.ImagePath = file.FileName;
+                            }
+                        }
+                    }
+                }
+
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TItle,ReleaseDate,Genre,Price,Rating,imagePath")] Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)            {
+                
+                try
+                {
+                    _context.Update(movie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(movie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movie = await _context.Movie.FindAsync(id);
+            //Delete Image from wwwroot/image
+            var movieImage = Path.Combine(_environment.WebRootPath, "images", movie.ImagePath);
+            if (System.IO.File.Exists(movieImage))
+            {
+                System.IO.File.Delete(movieImage);
+            }
+            _context.Movie.Remove(movie);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movie.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ MvcMovie/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(serviceProvider.GetRequiredService<DbContextOptions<MvcMovieContext>>()))
+            {
+                if (context.Movie.Any())
+                {
+                    return; // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        TItle = "Mountain of the Lord",
+                        ReleaseDate = DateTime.Parse("2005-3-12"),
+                        Genre = "Historical",
+                        Rating = "R",
+                        Price = 7.99M,
+                        ImagePath = "mountain.jpg"
+                    },
+
+                    new Movie
+                    {
+                        TItle = "The Book of Mormon Movie",
+                        ReleaseDate = DateTime.Parse("2020-11-8"),
+                        Genre = "Adventure",
+                        Rating = "PG",
+                        Price = 10.99M,
+                        ImagePath = "bom.jpg"
+                    },
+
+                    new Movie
+                    {
+                        TItle = "Mr. Krueger's Christmas",
+                        ReleaseDate = DateTime.Parse("2010-4-15"),
+                        Genre = "Fantasy",
+                        Rating = "R",
+                        Price = 5.99M,
+                        ImagePath = "christmas.jpg"
+                    },
+
+                    new Movie
+                    {
+                        TItle = "Pioneer in Petticoats",
+                        ReleaseDate = DateTime.Parse("2002-1-31"),
+                        Genre = "Drama",
+                        Rating = "PG",
+                        Price = 7.99M,
+                        ImagePath = "pioneer.jpg"
+                    },
+
+                    new Movie
+                    {
+                        TItle = "The Best Two Years",
+                        ReleaseDate = DateTime.Parse("2008-10-15"),
+                        Genre = "Adventure",
+                        Rating = "R",
+                        Price = 9.56M,
+                        ImagePath = "besttwoyears.jpg"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
